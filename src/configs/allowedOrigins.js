b08935a9ccb5786310e770b8a23945a77fb9f78f
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-19 22:13:27 -0300

    cors

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -5,6 +5,11 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://projeto17-shortly-front.vercel.app/",
+  "http://projeto17-shortly-front.vercel.app/",
+  "projeto17-shortly-front.vercel.app/",
+  "https://projeto17-shortly-front.vercel.app",
+  "http://projeto17-shortly-front.vercel.app",
+  "projeto17-shortly-front.vercel.app",
 ];
 
 export default allowedOrigins;
