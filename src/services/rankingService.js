0fd77d19204f8f630331ccb8c2859a278015bc74
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-21 13:23:19 -0300

    rankingRepository

--- src/services/rankingService.js
+++ src/services/rankingService.js
@@ -1,30 +1,5 @@
-import pool from "../configs/dbConn.js";
+import * as rankingRepository from "../repositories/rankingRepository.js";
 
 export const buildRanking = async () => {
-  const client = await pool.connect();
-  try {
-    const result = await client.query(
-      `SELECT
-        users.id,
-        users.name,
-        COUNT(urls.id) AS "linksCount",
-        COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
-    FROM
-        users
-    LEFT JOIN
-        urls ON users.id = urls."userId"
-    GROUP BY
-        users.id
-    ORDER BY
-        "visitCount" DESC
-    LIMIT
-        10;`
-    );
-    return result.rows;
-  } catch (err) {
-    console.error("Error building ranking", err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return rankingRepository.buildRanking();
 };
