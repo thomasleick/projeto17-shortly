da709759015ad1b7fa63d2c1bc92e89b17eb3b11
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-22 13:14:05 -0300

    nanoId validate with 404 status

--- src/middlewares/validateParams.js
+++ src/middlewares/validateParams.js
@@ -3,7 +3,7 @@ export const validateNanoIdAsParams = (req, res, next) => {
   const nanoidRegex = /^[a-zA-Z0-9_-]{6}$/;
 
   if (!nanoidRegex.test(id)) {
-    return res.status(400).json({
+    return res.status(404).json({
       message: "Invalid ID, must be a valid nanoid with 6 characters",
     });
   }
