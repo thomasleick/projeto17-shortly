c747607451251f91e58b2213eeeb104c3b447382
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-22 13:29:02 -0300

    back to a better configuration

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -47,7 +47,6 @@ export const handleLogin = async (req, res) => {
         name: foundUser.name,
         email: foundUser.email,
         accessToken,
-        token: accessToken //Remove after HUB NB
       });
     } else {
       return res.sendStatus(401); //unauthorized
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,12 +2,9 @@ import bcrypt from "bcrypt";
 import * as authRepository from "../repositories/authRepository.js";
 import pkg from "jsonwebtoken";
 const { sign, verify } = pkg;
-const ATK =
-  "60a067500b4ead6d69626ca013593343dd8ed6d14aa43240dcfe1b827facbc010dfe59656a8c99dd0fb1160c47cae63e3abc3abf298727d5dbb5dd399c0ae7b1";
-const RTK =
-  "aad009dbfb98cb71bb67cf0a90ee7a2b0f7074fdf12436fae349e9c0ca49398f60c96316da80d3fac6765a454ef7077239a72716e6201ed542d7d5fbf3be0502";
+
 export const refreshTokenExpiresIn = 24 * 60 * 60 * 1000; // one day
-export const accessTokenExpiresIn = 24 * 60 * 60 * 1000; //10 * 1000; // 10 seconds
+export const accessTokenExpiresIn = 10 * 1000; // 10 seconds
 
 export const comparePassword = async (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
@@ -22,17 +19,12 @@ export const generateTokens = (user) => {
         id: user.id,
       },
     },
-    ATK,
-    //process.env.ACCESS_TOKEN_SECRET,
+    process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: accessTokenExpiresIn }
   );
-  const refreshToken = sign(
-    { id: user.id },
-    RTK,
-    /* process.env.REFRESH_TOKEN_SECRET, */ {
-      expiresIn: refreshTokenExpiresIn,
-    }
-  );
+  const refreshToken = sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: refreshTokenExpiresIn,
+  });
   return { accessToken, refreshToken };
 };
 
@@ -41,13 +33,9 @@ export const saveRefreshToken = async (id, refreshToken) => {
 };
 
 const generateAccessToken = (userInfo) => {
-  return sign(
-    { UserInfo: userInfo },
-    ATK,
-    /* process.env.ACCESS_TOKEN_SECRET, */ {
-      expiresIn: accessTokenExpiresIn,
-    }
-  );
+  return sign({ UserInfo: userInfo }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: accessTokenExpiresIn,
+  });
 };
 
 export const verifyRefreshToken = async (refreshToken) => {
@@ -56,10 +44,7 @@ export const verifyRefreshToken = async (refreshToken) => {
     return false;
   }
   try {
-    const decoded = verify(
-      refreshToken,
-      RTK /* process.env.REFRESH_TOKEN_SECRET */
-    );
+    const decoded = verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
     if (foundUser.id !== decoded.id) {
       return false;
     }
